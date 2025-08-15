c5deb2a8ed03333cfe06aa7d043485f62057b4bc
daehan-lim <penjan.eng@gmail.com>
2025-08-15 11:10:51 +0900

    fix: Set ndkVersion to "27.2.12479018" and targetCompatibility to VERSION_17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,15 +8,15 @@ plugins {
 android {
     namespace = "com.daehan.film_mind"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.2.12479018"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
